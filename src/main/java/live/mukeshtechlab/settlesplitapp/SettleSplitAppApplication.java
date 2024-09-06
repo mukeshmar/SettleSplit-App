5ec01ac84ed234332b982bc49d1591a6688c9a3f
Mukesh Mar <mukeshmar0710@gmail.com>
2024-09-06 09:51:35 +0530

    First Commit

---
+++ src/main/java/live/mukeshtechlab/settlesplitapp/SettleSplitAppApplication.java
@@ -0,0 +1,13 @@
+package live.mukeshtechlab.settlesplitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleSplitAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettleSplitAppApplication.class, args);
+    }
+
+}
